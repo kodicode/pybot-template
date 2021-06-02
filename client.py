5577a303ee6dd3855a1f4bfe9d1257d0d5645f16
kodicode <85211295+kodicode@users.noreply.github.com>
2021-06-02 10:25:54 +0600

    Add files via upload

---
+++ client.py
@@ -0,0 +1,35 @@
+import discord
+from discord.ext import commands
+
+client = commands.Bot(command_prefix = ">")
+
+@client.event
+async def on_ready():
+	await client.change_presence(status=discord.Status.idle, activity=discord.Game('>help'))
+	print('Bot working, bitch!')
+
+@client.event
+async def on_member_join(member):
+	role = discord.utils.get(member.guild.roles, id = 780020533428224032)
+	await member.add_roles(role)
+
+@client.command()
+async def boom(ctx):
+	author = ctx.message.author
+	await ctx.channel.purge(limit = 1)
+	await ctx.send(f'{author}, взорвался!')
+
+@client.command()
+@commands.has_permissions(view_audit_log = True)
+async def clear(ctx, amount = 999999999999):
+	author = ctx.message.author
+	await ctx.channel.purge(limit = amount)
+	await ctx.send(f'{author}, очистил сообщения')
+
+@client.command()
+@commands.has_permissions(view_audit_log = True)
+async def kick(ctx, member: discord.Member, *, reason = "Нарушение правил"):
+	await ctx.channel.purge(limit = 1)
+	await member.kick(reason = reason)
+
+client.run('Nzk4ODg5NzU5MDgyNDE0MDgw.X_7mEQ.uB4OY1J4slqGAe3cB5bqlWMmjHs')
